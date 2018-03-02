19087f8c6d799e62f413cdd23b88860fe39367cb
Rob Cooper <sunscreem@gmail.com>
2018-03-02 20:14:05 +0000

    Initial bits

---
+++ src/LaravelFlickrGalleryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace sunscreem\laravelflickrgallery;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFlickrGalleryProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
