5e93999da9c08e452704b6b38974c5657b0877e9
Rob Cooper <sunscreem@gmail.com>
2018-03-03 18:03:16 +0000

    Added a temp dd

--- src/LaravelFlickrGalleryServiceProvider.php
+++ src/LaravelFlickrGalleryServiceProvider.php
@@ -13,6 +13,7 @@ class LaravelFlickrGalleryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        dd('hi - Im working fine locally but try me on the vm and I fail');
     }
 
     /**a
