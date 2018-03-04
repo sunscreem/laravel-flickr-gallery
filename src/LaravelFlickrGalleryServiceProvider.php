328cec6c49fa2f894c783b0fd688e11e8efbdffb
Rob Cooper <sunscreem@gmail.com>
2018-03-04 18:22:18 +0000

    removed my test dd

--- src/LaravelFlickrGalleryServiceProvider.php
+++ src/LaravelFlickrGalleryServiceProvider.php
@@ -13,7 +13,6 @@ class LaravelFlickrGalleryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        dd('Hey - Im working fine locally but try me on the vm and I fail');
     }
 
     /**a
