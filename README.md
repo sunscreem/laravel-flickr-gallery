b884301655df0b3ce7ca5716433e2c94d21b2c3b
Rob Cooper <sunscreem@gmail.com>
2018-03-18 17:28:00 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# laravel-flickr-gallery
+# Add a Flickr collection to your website
+
+Flickr has some great tools for uploading, storing and editing your photos. This package uses the flickr API to bring those photo to your own website.
+
+It includes an optional lightbox/swipeable photo gallery.
+
+## Installation
+
+First, install the package via composer:
+
+```bash
+composer require sunscreem/laravel-flickr-galler
+```
+
+The package will automatically register itself.
+
+Now publish the configuration file and example view:
+
+```bash
+php artisan vendor:publish --provider="Sunscreem\LaravelFlickrGallery\LaravelFlickrGalleryServiceProvider"
+```
+
+Now add the following route to your `routes\web.php` file:
+
+```php
+Route::get('/gallery', function () {
+     return view('vendor.laravel-flickr-gallery.simple-photo-gallery');
+ });
+```
+
+Finally perform the migration:
+
+```bash
+php artisan migrate
+```
+
+## Configuration
+
+Set your flickr user id, key and secret in your .env file.
+
+Instructions on how to find these are here. [TBC]
+
+```php 
+FLICKR_USER_ID_TO_FETCH=1234567890@N00
+FLICKR_KEY=[your-flickr-key]
+FLICKR_SECRET=[your-flickr-secret]
+```
+
+## Usage
+
+Using `php artisan` you can pull in the latest photos from Flickr:
+
+```bash
+php artisan flickr:pull
+```
+
+Finally, view the gallery on your website by visiting `your-site.com/gallery`.
+
+## Credits
+
+- [Robert Cooper](https://robertcooper.xyz)
+- [All Contributors](../../contributors)
+
+## Support 
+
+If you have found this package helpful please consider [buying Rob a beer](https://buyrobabeer.com)!
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+
+
+
+
+
--- src/views/simple-photo-gallery.blade.php
+++ src/views/simple-photo-gallery.blade.php
@@ -6,28 +6,80 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="//cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.min.css" type="text/css" rel="stylesheet" />
-    
-    <title>Photo Gallery</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/justifiedGallery/3.6.3/css/justifiedGallery.min.css" />
+    <link rel="stylesheet" href="https://cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.min.css" />
+    <link rel="stylesheet" href="https://cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.gallery.min.css" />
+
+    <title>Simple One Page Photo Gallery</title>
+
+    <style>
+        
+        /* used when showing the captions when an image is clicked/tapped */
+        .caption {
+            position: relative;
+            bottom: 5vh;
+            height: 5vh;
+            color: white;
+            background-color: rgba(0, 0, 0, 0.6);
+            text-align: center;
+            padding-top: 1vh;
+        }
+
+
+        /* workaround for scroll bars appearing because of captions showing */
+        .featherlight .featherlight-content {
+
+            overflow: hidden !important;
+        }
+        
+    </style>
 
 </head>
 <body>
 
     <div class="container">
-        <h1>Recent Flickr Photos</h1>
+
+        <h1 class="m-4 text-center">Recent Flickr Photos</h1>
     
-        <div class="row mt-4">
-            @foreach(\Sunscreem\LaravelFlickrGallery\FlickrPhoto::all()->take(64) as $photo)
-                <div class="col-2 mb-3">
-                    <a href="{{ $photo->src }}" data-featherlight="image">
-                        <img src="{{ $photo->thumbsrc }}" alt="{{ $photo->caption }}" class="img-fluid img-thumbnail">
+        <div id="photoGallery" 
+             data-featherlight-gallery
+             data-featherlight-filter="a">
+
+                @foreach(\Sunscreem\LaravelFlickrGallery\FlickrPhoto::take(64)->get() as $photo)
+                    <a href="{{ $photo->src }}" class="gallery">
+                        <img src="{{ $photo->thumbsrc }}" alt="{{ $photo->caption }}">
                     </a>
-                </div>
-            @endforeach
+                @endforeach
+
         </div>
+
+
     </div>
 
     <script src="//code.jquery.com/jquery-latest.js"></script>
-    <script src="//cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
-</body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/detect_swipe/2.1.4/jquery.detect_swipe.min.js"></script> <!-- enable swipe when viewing photos on touch enabled devices -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/justifiedGallery/3.6.3/js/jquery.justifiedGallery.min.js"></script> <!-- snazzy layout -->
+    <script src="https://cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.min.js"></script> <!-- Lightbox -->
+    <script src="https://cdn.rawgit.com/noelboss/featherlight/1.7.12/release/featherlight.gallery.min.js"></script> <!-- Extends lightbox to add the photo gallery functionality -->
+    <script>
+        // wait until justified Gallery has completed loading and sortting out all the images
+        $("#photoGallery").justifiedGallery({}).on('jg.complete', function () {
+        
+            // initiate the gallery when images are clicked/tapped
+            $('a.gallery').featherlightGallery({
+                previousIcon: '«',
+                nextIcon: '»',
+                galleryFadeIn: 300,
+                openSpeed: 300
+            });
+
+            // puts the alt text of the images as captions when opened
+            $.featherlight.prototype.afterContent = function() {
+                var caption = this.$currentTarget.find('img').attr('alt');
+                this.$instance.find('.caption').remove();
+                $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+            };
+        });
+    </script>
+    </body>
 </html>
